7f589e43db5971f4748b046f2311f27a3bdae334
Bluette1 <marylene.sawyer@gmail.com>
2022-01-13 06:49:55 +0200

    Modify data

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default class IndexRoute extends Route {
   async model() {
     let response = await fetch('/api/comments.json');
-    let { data } = await response.json();
+    let data = await response.json();
     return data;
   }
 }
