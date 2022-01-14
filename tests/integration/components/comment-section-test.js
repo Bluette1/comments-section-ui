675087eee934ff8d41e5d7dca725279e0de6a49e
Bluette1 <marylene.sawyer@gmail.com>
2022-01-14 14:43:25 +0200

    Fix lint errors

--- tests/integration/components/comment-section-test.js
+++ tests/integration/components/comment-section-test.js
@@ -7,25 +7,7 @@ module('Integration | Component | comment-section', function (hooks) {
   setupRenderingTest(hooks);
 
   test.skip('it renders', async function (assert) {
-   
-
-  const commentReply = {
-      "id": 1,
-      "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      "createdAt": "1 month ago",
-      "score": 12,
-      "user": {
-        "image": {
-          "png": "../images/avatars/image-amyrobson.png",
-          "webp": "../images/avatars/image-amyrobson.webp"
-        },
-        "username": "amyrobson"
-      },
-      "replies": []
-    }
- 
-    await render(hbs`<CommentSection @commentReply={{commentReply}} />`);
-    assert(true, true)
-     
+    await render(hbs`<CommentSection @commentReply=commentReply />`);
+    assert(true, true);
   });
 });
--- tests/integration/components/comment-test.js
+++ tests/integration/components/comment-test.js
@@ -10,20 +10,23 @@ module('Integration | Component | comment', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    this.setProperties({commentReply: {
-      "id": 1,
-      "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      "createdAt": "1 month ago",
-      "score": 12,
-      "user": {
-        "image": {
-          "png": "../images/avatars/image-amyrobson.png",
-          "webp": "../images/avatars/image-amyrobson.webp"
+    this.setProperties({
+      commentReply: {
+        id: 1,
+        content:
+          "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+        createdAt: '1 month ago',
+        score: 12,
+        user: {
+          image: {
+            png: '../images/avatars/image-amyrobson.png',
+            webp: '../images/avatars/image-amyrobson.webp',
+          },
+          username: 'amyrobson',
         },
-        "username": "amyrobson"
+        replies: [],
       },
-      "replies": []
-    }})
+    });
 
     await render(hbs`<Comment @comment={{commentReply}} />`);
     assert.dom('.comment-div').exists();
