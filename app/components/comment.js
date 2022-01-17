0666b2323eaa4fa47035426703e55786ff23abc3
Bluette1 <marylene.sawyer@gmail.com>
2022-01-18 01:34:52 +0200

    Add edit comment function

--- app/components/comment.js
+++ app/components/comment.js
@@ -4,6 +4,7 @@ import { action } from '@ember/object';
 
 export default class Comment extends Component {
   @tracked showTextBox = false;
+  @tracked showEditInput = false;
 
   get isReply() {
     const { reply } = this.args;
@@ -24,7 +25,14 @@ export default class Comment extends Component {
     return username === currUser;
   }
 
-  edit() {}
+  @action edit() {
+    this.showEditInput = !this.showEditInput;
+  }
+
+  get content() {
+    const { comment: { content } } = this.args;
+    return content;
+  }
 
   delete() {}
 }
