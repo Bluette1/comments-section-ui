af7f2bfbe2fd17a789b249d9ff311c14da1ef637
Bluette1 <marylene.sawyer@gmail.com>
2022-01-17 04:26:08 +0200

    Current User can input text

--- app/components/comment.js
+++ app/components/comment.js
@@ -1,8 +1,16 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class Comment extends Component {
+  @tracked showTextBox = false;
+
   get isReply() {
     const { reply } = this.args;
     return reply;
+  }
+
+  @action textInput() {
+    this.showTextBox = !this.showTextBox;
   }
 }
