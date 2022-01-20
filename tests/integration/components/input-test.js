4abe5047b5e51c0cc9b26f05d5c191213ce00d61
Bluette1 <marylene.sawyer@gmail.com>
2022-01-20 07:44:05 +0200

    Use .webp file extension
    - Solve Netlify image display issue

--- tests/integration/components/input-test.js
+++ tests/integration/components/input-test.js
@@ -10,7 +10,7 @@ module('Integration | Component | input', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Input />`);
+    await render(hbs`<InputBox />`);
 
     assert.dom(this.element).hasText('');
 
