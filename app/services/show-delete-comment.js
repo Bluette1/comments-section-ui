e1d9884ecd84f9ce69c136874ca9080dbce1238b
Bluette1 <marylene.sawyer@gmail.com>
2022-01-24 07:18:56 +0200

    Fix lint

--- app/services/show-delete-comment.js
+++ app/services/show-delete-comment.js
@@ -3,9 +3,9 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ShowDeleteCommentService extends Service {
- @tracked deleteComment = false;
+  @tracked deleteComment = false;
 
- @action showDelete() {
-    this.deleteComment = !this.deleteComment;  
+  @action showDelete() {
+    this.deleteComment = !this.deleteComment;
   }
 }
