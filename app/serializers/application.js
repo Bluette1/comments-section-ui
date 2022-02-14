3fd32d0d74acf4453fe1c403b66447e33e453537
Bluette1 <marylene.sawyer@gmail.com>
2022-02-14 08:18:51 +0200

    Fix models - retrieving data

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,26 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
 
-export default class ApplicationSerializer extends JSONAPISerializer {}
+// export default class ApplicationSerializer extends JSONAPISerializer {}
+export default JSONAPISerializer.extend({
+  
+  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+    console.log('payload DATA: ', payload.data)
+    if (Array.isArray(payload.data)) {
+      payload.data.forEach(data => {
+        data.attributes.webp = data.attributes.user.image.webp;
+        data.attributes.username = data.attributes.user.username;
+    
+        delete data.attributes.user;
+      });
+    } else {
+      payload.data.attributes.webp = payload.data.attributes.user.image.webp;
+      payload.data.attributes.username = payload.data.attributes.user.username;
+  
+      delete payload.data.attributes.user;
+
+    }
+
+   
+    return this._super(...arguments);
+  },
+})
